641b9bde39b118d7e00972734ab3a41bbf875b90
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2021-10-17 17:47:20 +0630

    integrate tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border-solid border border-indigo-400 h-10 w-40 bg-yellow-500 rounded-lg">
+      asdfasf
     </div>
   );
 }
